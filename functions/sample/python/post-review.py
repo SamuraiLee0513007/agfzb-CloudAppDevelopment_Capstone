27229d898ef9be9cfb91bf0061f2527f4a0ddf40
kaiser-sb <109263405+kaiser-sb@users.noreply.github.com>
2023-02-04 17:31:48 +0530

    Deleted api key and url

--- functions/sample/python/post-review.py
+++ functions/sample/python/post-review.py
@@ -7,9 +7,9 @@ import sys
 from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def main(dict):
-    authenticator = IAMAuthenticator("jIgFW3QzX8e_k8aAWhYA4SaeDfXQ62Jy0szogP5-eVJa")
+    authenticator = IAMAuthenticator("API KEY TO BE ENTERED")
     service = CloudantV1(authenticator=authenticator)
-    service.set_service_url("https://apikey-v2-rg5tzn3w5twkdi5w3oiiwrc8i3qlbhhmy8miw6ptjtx:11749eab45d913ef5e98f4276a578e76@148477bd-c1eb-4521-a003-97beae46f41d-bluemix.cloudantnosqldb.appdomain.cloud")
+    service.set_service_url("URL TO BE ENTERED")
     response = service.post_document(db='reviews', document=dict["review"]).get_result()
     try:
     # result_by_filter=my_database.get_query_result(selector,raw_result=True)
