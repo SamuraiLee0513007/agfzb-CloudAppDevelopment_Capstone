eac829a36b25c2ca744fbaa7a05962c04a951466
SamuraiLee0513007 <cyleeax@connect.ust.hk>
2023-06-10 18:38:33 -0400

    AWS fixes

--- functions/sample/nodejs/get-dealership.js
+++ functions/sample/nodejs/get-dealership.js
@@ -55,7 +55,7 @@ function main(params) {
             })
         } else {
             // return all documents 
-            cloudant.postAllDocs({ db: 'dealerships', includeDocs: true, limit: 10 })            
+            cloudant.postAllDocs({ db: 'dealerships', includeDocs: true})            
             .then((result)=>{
               // console.log(result.result.rows);
               let code = 200;
